1da9c9425b2902cb8c16c3623f08d58ffaf7fe61
DootDoot4567 <dootdoot4567@gmail.com>
2026-02-11 19:46:39 -0600

    Fix redeclaration issue on decoder

--- decoder.sv
+++ decoder.sv
@@ -21,28 +21,28 @@ module decoder(
 );
     logic [31:0] instr;
 
-    logic isALUreg = (instr[6:0] == 7'b0110011);
-    logic isALUimm = (instr[6:0] == 7'b0010011);
-    logic isBranch = (instr[6:0] == 7'b1100011);
-    logic isJALR   = (instr[6:0] == 7'b1100111);
-    logic isJAL    = (instr[6:0] == 7'b1101111);
-    logic isAUIPC  = (instr[6:0] == 7'b0010111);
-    logic isLUI    = (instr[6:0] == 7'b0110111);
-    logic isLoad   = (instr[6:0] == 7'b0000011);
-    logic isStore  = (instr[6:0] == 7'b0100011);
-    logic isSYSTEM = (instr[6:0] == 7'b1110011);
+    assign isALUreg = (instr[6:0] == 7'b0110011);
+    assign isALUimm = (instr[6:0] == 7'b0010011);
+    assign isBranch = (instr[6:0] == 7'b1100011);
+    assign isJALR   = (instr[6:0] == 7'b1100111);
+    assign isJAL    = (instr[6:0] == 7'b1101111);
+    assign isAUIPC  = (instr[6:0] == 7'b0010111);
+    assign isLUI    = (instr[6:0] == 7'b0110111);
+    assign isLoad   = (instr[6:0] == 7'b0000011);
+    assign isStore  = (instr[6:0] == 7'b0100011);
+    assign isSYSTEM = (instr[6:0] == 7'b1110011);
 
-    logic [4:0] rs1Id = instr[19:15];
-    logic [4:0] rs2Id = instr[24:20];
-    logic [4:0] rdId  = instr[11:7];
+    assign rs1Id  instr[19:15];
+    assign rs2Id = instr[24:20];
+    assign rdId  = instr[11:7];
 
-    logic [2:0] funct3 = instr[14:12];
-    logic [6:0] funct7 = instr[31:25];
+    assign funct3 = instr[14:12];
+    assign funct7 = instr[31:25];
 
-    logic [31:0] Uimm = { instr[31], instr[30:12], 12'b0 };
-    logic [31:0] Iimm = {{21{instr[31]}}, instr[30:20]};
-    logic [31:0] Simm = {{21{instr[31]}}, instr[30:25], instr[11:7]};
-    logic [31:0] Bimm = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
-    logic [31:0] Jimm = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
+    assign Uimm = { instr[31], instr[30:12], 12'b0 };
+    assign Iimm = {{21{instr[31]}}, instr[30:20]};
+    assign Simm = {{21{instr[31]}}, instr[30:25], instr[11:7]};
+    assign Bimm = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
+    assign Jimm = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
 
 endmodule
