24da518ccdf6aa47d3284e7974cfe11e753b7102
DootDoot4567 <dootdoot4567@gmail.com>
2026-02-22 20:46:16 -0600

    Add BRAM signals to processor module. Add initial design of address mask for load and store. Fix pc increment bug by incrementing pc in exec state. Add new signals to be scheduled in the state machine

--- processor.sv
+++ processor.sv
@@ -6,9 +6,20 @@ module processor #(
 );
     logic [31:0] pc = 0;
 
+    //BRAM inputs and outputs being declared as logic
+
+    // logic clockWrite = 0;
+    // logic clockRead = 0;
+    logic writeEnable = 0;
+    logic readEnable = 0;
+    logic [6:0] addrRead = 0;
+    // logic [6:0] addrWrite = 0;
     logic [31:0] dataOut;
+    logic [31:0] dataIn;
+
     logic [31:0] instr;
 
+    //Boolean flags used by the decoder, processor, and alu
     logic isALUreg;
     logic isALUimm;
     logic isBranch;
@@ -20,10 +31,12 @@ module processor #(
     logic isStore;    
     logic isSYSTEM;
 
+    //Indexes for input registers and ra register
     logic [4:0] rs1Id;
     logic [4:0] rs2Id;
     logic [4:0] rdId;
 
+    //Optional opcode fields for instruction
     logic [2:0] funct3;
     logic [6:0] funct7;
 
@@ -36,7 +49,7 @@ module processor #(
     logic [31:0] rs1;
     logic [31:0] rs2;
 
-    logic [31:0] registerFile [31:0];
+    logic [31:0] registerFile [0:31];
 
     initial 
         begin
@@ -45,13 +58,12 @@ module processor #(
 
     logic [31:0] aluOut;
     logic [31:0] writeBackData;
-
     logic writeBackEnable = 0;
-    logic readEnable = 0;
 
     logic [31:0] nextPcCandidate;
     logic [31:0] writeBackDataCandidate;
 
+    logic [31:0] memAddr;
 
     localparam HALT = 3'b000;
     localparam INITIAL = 3'b001;
@@ -65,16 +77,16 @@ module processor #(
 
     bram_sdp #(
         .WIDTH(32), 
-        .DEPTH(128),
+        .DEPTH(73),
         .INIT(INIT)
     ) bram_inst (
-        .clockWrite(1'b0),
+        .clockWrite(clock),
         .clockRead(clock),
-        .writeEnable(1'b0),
-        .readEnable(readEnable),
+        .writeEnable,
+        .readEnable,
         .addrWrite(7'b0),
-        .addrRead(pc[8:2]),
-        .dataIn(0),
+        .addrRead(addrRead),
+        .dataIn,
         .dataOut(dataOut)
     );
 
@@ -128,7 +140,30 @@ module processor #(
         .writeBackDataCandidate,
         .nextPcCandidate
     );
-   
+
+    // assign memAddr = rs1 + (isLoad ? Iimm : Simm);
+
+    // logic [7:0] byteData = memAddr[1:0] == 0 ? dataOut[7:0] :
+    //                    memAddr[1:0] == 1 ? dataOut[15:8] :
+    //                    memAddr[1:0] == 2 ? dataOut[23:16] :
+    //                                         dataOut[31:24];
+
+    // logic [31:0] loadData = {{24{byteData[7]}}, byteData};
+    // logic [31:0] storeWord = dataOut;
+
+    // always @(*)
+    //     begin
+    //         if (funct3 == 3'b000)
+    //             begin
+    //                 case(memAddr[1:0])
+    //                     0: storeWord[7:0]   = rs2[7:0];
+    //                     1: storeWord[15:8]  = rs2[7:0];
+    //                     2: storeWord[23:16] = rs2[7:0];
+    //                     3: storeWord[31:24] = rs2[7:0];
+    //                 endcase
+    //             end
+    //     end
+
     always @(posedge clock) 
         begin
             case(state)
@@ -136,74 +171,108 @@ module processor #(
                     begin
                         if (reset) 
                             begin
-                                pc <= 12;
-                                writeBackEnable <= 0;
+                                pc <= 0;
                                 state <= INITIAL;
-                            end
-                        else
-                            begin
-                                state <= HALT;
                             end
                     end
                 INITIAL:
                     begin
+
+                        //Set up signals for fetch state 
+
                         readEnable <= 1;
+                        //we use the second bit because we increment by 4 for the pc. the 2 bit corresponds to the first line in our .mem file
+                        addrRead <= pc[8:2];
+                        writeBackEnable <= 0;
+
                         state <= FETCH;
                     end
                 FETCH:
                     begin
-                        readEnable <= 1;
+                        $display("FETCH");
+
+                        //Schedule readEnable to go down instant of next clock cycle
+                        readEnable <= 0;
+
+                        addrRead <= nextPcCandidate[8:2];
+
                         state <= DECODE;
                     end
                 DECODE: 
                     begin
-                        instr <= dataOut;
-                        readEnable <= 0;
+                        $display("DECODE");
+
+                        //calculate branch and jump targets here
+                        instr <= dataOut; 
+                        //instr <= 32'h123450b7;
+
                         rs1 <= registerFile[rs1Id];
                         rs2 <= registerFile[rs2Id];
+
                         state <= EXECUTE;
                     end
                 EXECUTE: 
                     begin
-                        if (!isSYSTEM) 
+                        $display("EXEC");
+                        //if its branch or jump then update pc 
+
+                        //if load or a store, then read or write enable goes up so that its ready for mem state
+
+                        if (!isSYSTEM)
                             begin
-                                if (!isSYSTEM)
+                                if (pc === 288)
                                     begin
-                                        if (pc == 288)
-                                            begin
-                                                pc <= 0;
-                                            end
-                                        else
-                                            begin
-                                                pc <= nextPcCandidate;
-                                            end
+                                        pc <= 0;
+                                    end
+                                else
+                                    begin
+                                        pc <= nextPcCandidate;
                                     end
                             end
+
+                        writeEnable <= 1;
                         
                         state <= MEMORY;	          
                     end
                 MEMORY:
                     begin
+                        $display("MEM");
+                        //schedule so that read and write enable to go down
+                        //writes to mem
+
+                        if(isLoad) 
+                            begin
+                                
+                            end
+
+                        writeBackData <= writeBackDataCandidate;
+
+                        writeBackEnable <= (!isBranch && !isStore && !isLoad);
+
+                        readEnable <= 0;
+                        writeEnable <= 0;
+
                         state <= WRITE_BACK;
                     end
                 WRITE_BACK:
                     begin
-                        writeBackData <= writeBackDataCandidate;
-
-                        writeBackEnable <= (!isBranch) & (!isStore);
+                        $display("WB");
+                        //writes to register file
 
                         if(writeBackEnable && rdId != 0) 
                             begin
                                 registerFile[rdId] <= writeBackData;
-                
-                                // `ifdef SIMULATION	 
-                                //          $display("x%0d <= %b",rdId,writeBackData);
-                                // `endif	 
+
+                                `ifdef SIMULATION	 
+                                          $display("x%0d <= %b",rdId,writeBackData);
+                                `endif
                             end
-                        
+
+                        writeBackEnable <= 0;
+                        readEnable <= 1;
                         state <= FETCH;
                     end
-            endcase 
+            endcase
         end
 
     `ifdef SIMULATION
