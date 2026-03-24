3de9b7b54e0e6679b26d6fe69f11b7d8e2b1dc44
DootDoot4567 <dootdoot4567@gmail.com>
2026-03-24 14:56:01 -0500

    Instantiate both the processor and BRAM in the soc, as well as consolidate ADDR_WIDTH computation in one place to be passed as a parameter

--- soc.sv
+++ soc.sv
@@ -4,42 +4,52 @@ module soc #(
     parameter DEPTH = 16384
 ) (
     input logic clock,
-    input logic reset)
+    input logic reset
 );
+
+    //Computes minimum bits needed for the mem addresses using log_2(depth)
+    localparam ADDR_WIDTH=$clog2(DEPTH);
+
     //BRAM inputs and outputs being declared as logic
+    logic writeEnable;
+    logic readEnable;
+    logic [ADDR_WIDTH - 1:0] addrRead;
+    logic [ADDR_WIDTH - 1:0] addrWrite;
+    logic [WIDTH - 1:0] dataIn;
+    logic [WIDTH - 1:0] dataOut;
 
     //Instatiate the processor
     processor #(
-        .INIT(INIT)
+        .INIT(INIT),
         .WIDTH(WIDTH),
-        .DEPTH(DEPTH)
+        .DEPTH(DEPTH),
+        .ADDR_WIDTH(ADDR_WIDTH)
     ) processor_inst (
         .clock,
         .reset,
-        .clockWrite(clock),
-        .clockRead(clock),
+        .dataOut,
         .writeEnable,
         .readEnable,
-        .addrWrite(addrWrite),
-        .addrRead(addrRead),
-        .dataIn(dataIn),
-        .dataOut(dataOut)
+        .addrWrite,
+        .addrRead,
+        .dataIn
     );
 
     //Instatiate the BRAM (simple dual port)
     bram_sdp #(
         .WIDTH(WIDTH),
         .DEPTH(DEPTH),
+        .ADDR_WIDTH(ADDR_WIDTH),
         .INIT(INIT)
     ) bram_inst (
         .clockWrite(clock),
         .clockRead(clock),
         .writeEnable,
         .readEnable,
-        .addrWrite(addrWrite),
-        .addrRead(addrRead),
-        .dataIn(dataIn),
-        .dataOut(dataOut)
+        .addrWrite,
+        .addrRead,
+        .dataIn,
+        .dataOut
     );
 
 endmodule
