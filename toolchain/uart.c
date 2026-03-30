ed3d4bbe8717d77106835c9cef8691088d6afeb7
DootDoot4567 <dootdoot4567@gmail.com>
2026-03-30 12:33:42 -0500

    Fix type for UART RX, TX, and STATUS

--- toolchain/uart.c
+++ toolchain/uart.c
@@ -4,9 +4,9 @@
 #define IO_DEVICE(x) (IO_BASE + x * 0x40)
 
 //Define memory mapping at 0x240, 0x241, 0x242, and leave 0x243 unused
-#define UART_RX(x) (*(volatile * uint8_t) (IO_DEVICE(x) + 0x0))
-#define UART_TX(x) (*(volatile * uint8_t) (IO_DEVICE(x) + 0x1))
-#define UART_STATUS(x) (*(volatile * uint8_t) (IO_DEVICE(x) + 0x2))
+#define UART_RX(x) (* (volatile uint8_t *) (IO_DEVICE(x) + 0x0))
+#define UART_TX(x) (* (volatile uint8_t *) (IO_DEVICE(x) + 0x1))
+#define UART_STATUS(x) (* (volatile uint8_t *) (IO_DEVICE(x) + 0x2))
 
 //Define status signals for the uart receiver and transmitter
 //Fits within one integer register 
