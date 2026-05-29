b5ab6cdb3bba1ceca6da41142507dc8ad41cb5e0
DootDoot4567 <dootdoot4567@gmail.com>
2026-05-29 18:42:10 -0500

    Upload register forwarder module

---
+++ hardware/reg_forwarder.sv
@@ -0,0 +1,50 @@
+module reg_forwarder (
+    input  logic [4:0] e_rs1Id,
+    input  logic [4:0] e_rs2Id,
+    input  logic [31:0] de_rs1,
+    input  logic [31:0] de_rs2,
+    input  logic [4:0] em_rdId,
+    input  logic [31:0] em_writeBackData,
+    input  logic        em_writesRd,
+    input  logic [4:0] mw_rdId,
+    input  logic [31:0] wb_writeData,
+    input  logic        mw_writesRd,
+    output logic [31:0] e_rs1Forwarded,
+    output logic [31:0] e_rs2Forwarded,
+
+    output logic em_fwd_rs1,
+    output logic ew_fwd_rs1,
+    output logic em_fwd_rs2,
+    output logic ew_fwd_rs2
+);
+    assign em_fwd_rs1 = (em_rdId != 0) && em_writesRd && (em_rdId == e_rs1Id);
+    assign em_fwd_rs2 = (em_rdId != 0) && em_writesRd && (em_rdId == e_rs2Id);
+
+    assign ew_fwd_rs1 = (mw_rdId != 0) && mw_writesRd && (mw_rdId == e_rs1Id);
+    assign ew_fwd_rs2 = (mw_rdId != 0) && mw_writesRd && (mw_rdId == e_rs2Id);
+
+    always_comb begin
+        if (em_fwd_rs1) begin
+            e_rs1Forwarded = em_writeBackData;
+        end
+        else if (ew_fwd_rs1) begin
+            e_rs1Forwarded = wb_writeData;
+        end
+        else begin
+            e_rs1Forwarded = de_rs1;
+        end
+    end
+
+    always_comb begin
+        if (em_fwd_rs2) begin
+            e_rs2Forwarded = em_writeBackData;
+        end
+        else if (ew_fwd_rs2) begin
+            e_rs2Forwarded = wb_writeData;
+        end
+        else begin
+            e_rs2Forwarded = de_rs2;
+        end
+    end
+
+endmodule
